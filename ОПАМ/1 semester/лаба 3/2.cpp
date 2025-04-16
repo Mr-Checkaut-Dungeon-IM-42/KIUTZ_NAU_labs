fc491056701896992c501710babada29fc127ced
Ivan <ivan.r.fedorenko@gmail.com>
2025-04-16 22:23:59 +0300

    OPAM/1sem/lab 3 rewrote

--- ОПАМ/1 semester/лаба 3/2.cpp
+++ ОПАМ/1 semester/лаба 3/2.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
 #include <string>
-#include <math.h>
+#include <map>
 using namespace std;
 
 int main() 
 {
     setlocale(LC_CTYPE, "ukr");
 
+    map<string, int> months = {
+        {"Січень", 1},
+        {"Лютий", 1},
+        {"Березень", 1},
+        {"Квітень", 2},
+        {"Травень", 2},
+        {"Червень", 2},
+        {"Липень", 3},
+        {"Серпень", 3},
+        {"Вересень", 3},
+        {"Жовтень", 4},
+        {"Листопад", 4},
+        {"Грудень", 4}
+    };
+
     string myMonth;
     cin >> myMonth;
 
-    if(myMonth == "Січень") {
-        cout << "Січень, перший квартал";
-    } else if(myMonth == "Лютий") {
-        cout << "Лютий, перший квартал";
-    } else if(myMonth == "Березень") {
-        cout << "Березень, перший квартал";
-    } else if(myMonth == "Квітень") {
-        cout << "Квітень, другий квартал";
-    } else if(myMonth == "Травень") {
-        cout << "Травень, другий квартал";
-    } else if(myMonth == "Червень") {
-        cout << "Червень, другий квартал";
-    } else if(myMonth == "Липень") {
-        cout << "Липень, третій квартал";
-    } else if(myMonth == "Серпень") {
-        cout << "Серпень, третій квартал";
-    } else if(myMonth == "Вересень") {
-        cout << "Вересень, третій квартал";
-    } else if(myMonth == "Жовтень") {
-        cout << "Жовтень, четвертий квартал";
-    } else if(myMonth == "Листопад") {
-        cout << "Листопад, четвертий квартал";
-    } else if(myMonth == "Грудень") {
-        cout << "Грудень, четвертий квартал";
+    if (months.find(myMonth) != months.end()) {
+        int quarter = months[myMonth];
+        string quarterName;
+        switch (quarter) {
+            case 1: quarterName = "перший"; break;
+            case 2: quarterName = "другий"; break;
+            case 3: quarterName = "третій"; break;
+            case 4: quarterName = "четвертий"; break;
+        }
+        cout << myMonth << ", " << quarterName << " квартал";
     } else {
         cout << "Вашого місяця не знайдено";
     }
